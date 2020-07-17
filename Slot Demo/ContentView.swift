5da0faae4d0d0a35ccee7b2facd37c68665f54d2
Arpit <arpit.choudhary@timesinternet.in>
2020-07-17 14:43:29 +0530

    Initial Commit

---
+++ Slot Demo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Slot Demo
+//
+//  Created by Arpit Choudhary on 17/07/20.
+//  Copyright © 2020 Arpit Choudhary. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
